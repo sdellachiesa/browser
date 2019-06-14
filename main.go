b82832a7850207a87f410840bf645b7981de51a0
Martin Palma <martin.palma@eurac.edu>
2019-06-14 09:50:09 +0200

    browser: editing flags and their description.

--- main.go
+++ main.go
@@ -17,23 +17,24 @@ import (
 // TODO: check influx db error response
 // TODO: fmt.Sprintf %s for queries composing is not safe (SQL injection)
 // TODO: add key to requests for preventing request forgery
-// TODO: check on Cross-origin resource sharing (CORS
+// TODO: check on Cross-origin resource sharing (CORS)
+
+const defaultAddr = "localhost:8888" // default webserver address
 
 func main() {
 	log.SetPrefix("browser: ")
 
 	fs := flag.NewFlagSet("browser", flag.ExitOnError)
 	var (
-		listenAddr = fs.String("listenAddr", "localhost:8888", "")
-		influxAddr = fs.String("influx-host", "http://127.0.0.1:8086", "Influx host:port.")
-		influxUser = fs.String("influx-username", "", "Influx username.")
-		influxPass = fs.String("influx-password", "", "Influx password.")
+		httpAddr   = fs.String("http", defaultAddr, "HTTP service address")
+		influxAddr = fs.String("influx-addr", "http://127.0.0.1:8086", "Influx protocol:\\host:port")
+		influxUser = fs.String("influx-username", "", "Influx username")
+		influxPass = fs.String("influx-password", "", "Influx password")
 		dbName     = fs.String("influx-database", "lter", "Influx database name")
-		//devMode       = fs.Bool("dev", false, "run in dev mode")
 		//oauthClientID = fs.String("oauth-clientid", "", "")
 		//oauthSecret   = fs.String("oauth-secret", "", "")
 		//oauthRedirect = fs.String("oauth-redirect", "", "")
-		_ = fs.String("config", "", "config file (optional)")
+		_ = fs.String("config", "", "Config file (optional)")
 	)
 
 	ff.Parse(fs, os.Args[1:],
@@ -66,7 +67,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Fatal(http.ListenAndServe(*listenAddr, s))
+	log.Fatal(http.ListenAndServe(*httpAddr, s))
 }
 
 func generateKey() (string, error) {
