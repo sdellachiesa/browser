7f929c3114ecade4fbe8adcbbd0d4d65bde9c3ce
Martin Palma <martin.palma@eurac.edu>
2019-09-03 08:40:08 +0200

    internal/middleware: fix comment typo

--- internal/browser/middleware.go
+++ internal/browser/middleware.go
@@ -2,8 +2,8 @@ package browser
 
 import "net/http"
 
-// Headers is a http middleware which appends default secuirty headers
-// to the http response before calling the next http.Handler.
+// Headers is a http middleware which appends default security headers
+// to the http response before calling the next handler.
 func Headers(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
