f4e483c97c06a4f8a30d69b40b2d34234cafe904
Martin Palma <martin.palma@eurac.edu>
2020-09-23 09:52:32 +0200

    internal/http: move SecureHeader middleware to dedicated file

--- internal/http/http.go
+++ internal/http/http.go
@@ -78,19 +78,3 @@ func isAllowed(r *http.Request, roles ...browser.Role) bool {
 
 	return false
 }
-
-// SecureHeaders adds security-related headers to all responses.
-func SecureHeaders() Middleware {
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Don't allow frame embedding.
-			w.Header().Set("X-Frame-Options", "deny")
-			// Prevent MIME sniffing.
-			w.Header().Set("X-Content-Type-Options", "nosniff")
-			// Block cross-site scripting attacks.
-			w.Header().Set("X-XSS-Protection", "1; mode=block")
-
-			h.ServeHTTP(w, r)
-		})
-	}
-}
